8fe274fa612284049180b306e1d61e2f10643045
Konstantin Sokolov <vtqveant@gmail.com>
2022-06-11 11:58:14 +0300

    eda done

---
+++ src/task2.py
@@ -0,0 +1,41 @@
+"""
+Можно свести классы к двухуровневой классификации,
+на первом уровне:
+
+    Manager, IT, Analyst, Blue Collar
+
+    Manager:
+    ------
+    General and Operations Managers,11-1021.00
+    Marketing Managers,11-2021.00
+    Sales Managers,11-2022.00
+    "Financial Managers, Branch or Department",11-3031.02
+    Retail Salespersons,41-2031.00
+    Customer Service Representatives,43-4051.00
+
+    IT:
+    ------
+    "Software Developers, Applications",15-1132.00
+    "Software Developers, Systems Software",15-1133.00
+    Web Developers,15-1134.00
+    Network and Computer Systems Administrators,15-1142.00
+    Computer User Support Specialists,15-1151.00
+
+    Analyst
+    ------
+    Management Analysts,13-1111.00
+    Financial Analysts,13-2051.00
+    Computer Systems Analysts,15-1121.00
+    Information Security Analysts,15-1122.00
+    Intelligence Analysts,33-3021.06
+
+    Blue Collar
+    ------
+    Registered Nurses,29-1141.00
+    Nursing Assistants,31-1014.00
+    Automotive Specialty Technicians,49-3023.02
+    "Maintenance and Repair Workers, General",49-9071.00
+    Heavy and Tractor-Trailer Truck Drivers,53-3032.00
+
+
+"""
